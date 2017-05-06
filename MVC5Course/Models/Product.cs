b795ef56676718015668cda3611fccf8be8fa7f2
danny <danny75@gmail.com>
2017-05-06 14:45:14 +0800

    12 練習基本的模型繫結，將 /Products/Index 傳入 Active 參數以篩選資料輸出

--- MVC5Course/Models/Product.cs
+++ MVC5Course/Models/Product.cs
@@ -11,8 +11,7 @@ namespace MVC5Course.Models
 {
     using System;
     using System.Collections.Generic;
-    using System.ComponentModel.DataAnnotations;
-
+    
     public partial class Product
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -22,16 +21,9 @@ namespace MVC5Course.Models
         }
     
         public int ProductId { get; set; }
-        [Required(ErrorMessage = "請輸入商品名稱")]
-        [RegularExpression("(.+)-(.+)",ErrorMessage ="格式錯誤")]
         public string ProductName { get; set; }
-        [Required]
-        [Range(0,9999,ErrorMessage = "請設定正確的商品價格範圍")]
         public Nullable<decimal> Price { get; set; }
-        [Required]
         public Nullable<bool> Active { get; set; }
-        [Required]
-        [Range(0, 100, ErrorMessage = "請設定正確的商品庫存量範圍")]
         public Nullable<decimal> Stock { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
--- MVC5Course/Models/Product.partial.cs
+++ MVC5Course/Models/Product.partial.cs
@@ -1,28 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Web;
-
 namespace MVC5Course.Models
 {
-	[MetadataType(typeof(ProductPartial))]
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    
+    [MetadataType(typeof(ProductMetaData))]
     public partial class Product
     {
     }
-
-    public partial class ProductPartial
+    
+    public partial class ProductMetaData
     {
-        [Required(ErrorMessage = "請輸入商品名稱")]
-        [RegularExpression("(.+)-(.+)", ErrorMessage = "格式錯誤")]
+        [Required]
+        public int ProductId { get; set; }
+        [Required]
+        [StringLength(80, ErrorMessage="欄位長度不得大於 80 個字元")]
         public string ProductName { get; set; }
-        [Required]
-        [Range(0, 9999, ErrorMessage = "請設定正確的商品價格範圍")]
+        [DisplayFormat(DataFormatString = "{0:0}",ApplyFormatInEditMode = true)]
         public Nullable<decimal> Price { get; set; }
-        [Required]
         public Nullable<bool> Active { get; set; }
-        [Required]
-        [Range(0, 100, ErrorMessage = "請設定正確的商品庫存量範圍")]
         public Nullable<decimal> Stock { get; set; }
+    
+        public virtual ICollection<OrderLine> OrderLine { get; set; }
     }
 }
